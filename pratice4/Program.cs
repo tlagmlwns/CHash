266224ecc24275fe56b90e6cf6b05bfc95fbb800
심희준 <simh2jun@gmail.com>
2023-07-20 16:54:35 +0900

    인터페이스 연습

---
+++ pratice4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace pratice4
+{
+    internal class Program
+    {
+        interface IRunnable
+        {
+            void Run();
+        }
+        interface IFlyable
+        {
+            void Fly();
+        }
+        class FlyCar : IRunnable, IFlyable
+        {
+            public void Run() { Console.WriteLine("Run~Run~"); }
+            public void Fly() { Console.WriteLine("Fly! Fly!"); }
+        }
+        static void Main(string[] args)
+        {
+            FlyCar car = new FlyCar();
+            car.Run();
+            car.Fly();
+
+            //IRunnable runnable = ((IRunnable)car);
+            //IFlyable flyable = ((IFlyable)car);
+            IRunnable runnable = car as IRunnable;
+            IFlyable flyable = car as IFlyable;
+            runnable.Run();
+            flyable.Fly();
+            //((IRunnable)car).Run();
+            //((IFlyable)car).Fly();
+        }
+    }
+}
