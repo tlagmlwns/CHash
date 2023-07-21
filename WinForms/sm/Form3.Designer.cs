0d49e8118a4e2f7a6c2d7733b3a92c7ae63b3c21
심희준 <simh2jun@gmail.com>
2023-07-21 11:42:16 +0900

    폼3 임시저장

--- WinForms/sm/Form3.Designer.cs
+++ WinForms/sm/Form3.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lb_test = new System.Windows.Forms.Label();
+            this.lb_Test = new System.Windows.Forms.Label();
             this.lb_Contain = new System.Windows.Forms.Label();
             this.lb_Equals = new System.Windows.Forms.Label();
             this.lb_Length = new System.Windows.Forms.Label();
@@ -37,24 +37,26 @@
             this.lb_ToLower = new System.Windows.Forms.Label();
             this.lb_ToUpper = new System.Windows.Forms.Label();
             this.lb_Trim = new System.Windows.Forms.Label();
-            this.lb_Split = new System.Windows.Forms.Label();
+            this.lb_Split1 = new System.Windows.Forms.Label();
             this.lb_Split2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lb_test
+            // lb_Test
             // 
-            this.lb_test.AutoSize = true;
-            this.lb_test.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lb_test.Location = new System.Drawing.Point(42, 32);
-            this.lb_test.Name = "lb_test";
-            this.lb_test.Size = new System.Drawing.Size(254, 30);
-            this.lb_test.TabIndex = 0;
-            this.lb_test.Text = "(label)Winform_StringTest";
+            this.lb_Test.AutoSize = true;
+            this.lb_Test.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_Test.Location = new System.Drawing.Point(42, 32);
+            this.lb_Test.Name = "lb_Test";
+            this.lb_Test.Size = new System.Drawing.Size(254, 30);
+            this.lb_Test.TabIndex = 0;
+            this.lb_Test.Text = "(label)Winform_StringTest";
             // 
             // lb_Contain
             // 
             this.lb_Contain.AutoSize = true;
-            this.lb_Contain.Location = new System.Drawing.Point(47, 87);
+            this.lb_Contain.Location = new System.Drawing.Point(47, 110);
             this.lb_Contain.Name = "lb_Contain";
             this.lb_Contain.Size = new System.Drawing.Size(11, 12);
             this.lb_Contain.TabIndex = 1;
@@ -63,7 +65,7 @@
             // lb_Equals
             // 
             this.lb_Equals.AutoSize = true;
-            this.lb_Equals.Location = new System.Drawing.Point(47, 129);
+            this.lb_Equals.Location = new System.Drawing.Point(47, 152);
             this.lb_Equals.Name = "lb_Equals";
             this.lb_Equals.Size = new System.Drawing.Size(11, 12);
             this.lb_Equals.TabIndex = 2;
@@ -72,7 +74,7 @@
             // lb_Length
             // 
             this.lb_Length.AutoSize = true;
-            this.lb_Length.Location = new System.Drawing.Point(47, 170);
+            this.lb_Length.Location = new System.Drawing.Point(47, 193);
             this.lb_Length.Name = "lb_Length";
             this.lb_Length.Size = new System.Drawing.Size(11, 12);
             this.lb_Length.TabIndex = 3;
@@ -81,7 +83,7 @@
             // lb_Replace
             // 
             this.lb_Replace.AutoSize = true;
-            this.lb_Replace.Location = new System.Drawing.Point(47, 208);
+            this.lb_Replace.Location = new System.Drawing.Point(47, 231);
             this.lb_Replace.Name = "lb_Replace";
             this.lb_Replace.Size = new System.Drawing.Size(11, 12);
             this.lb_Replace.TabIndex = 4;
@@ -90,7 +92,7 @@
             // lb_Substring
             // 
             this.lb_Substring.AutoSize = true;
-            this.lb_Substring.Location = new System.Drawing.Point(260, 87);
+            this.lb_Substring.Location = new System.Drawing.Point(260, 110);
             this.lb_Substring.Name = "lb_Substring";
             this.lb_Substring.Size = new System.Drawing.Size(11, 12);
             this.lb_Substring.TabIndex = 1;
@@ -99,7 +101,7 @@
             // lb_ToLower
             // 
             this.lb_ToLower.AutoSize = true;
-            this.lb_ToLower.Location = new System.Drawing.Point(260, 129);
+            this.lb_ToLower.Location = new System.Drawing.Point(260, 152);
             this.lb_ToLower.Name = "lb_ToLower";
             this.lb_ToLower.Size = new System.Drawing.Size(11, 12);
             this.lb_ToLower.TabIndex = 2;
@@ -109,7 +111,7 @@
             // lb_ToUpper
             // 
             this.lb_ToUpper.AutoSize = true;
-            this.lb_ToUpper.Location = new System.Drawing.Point(260, 170);
+            this.lb_ToUpper.Location = new System.Drawing.Point(260, 193);
             this.lb_ToUpper.Name = "lb_ToUpper";
             this.lb_ToUpper.Size = new System.Drawing.Size(11, 12);
             this.lb_ToUpper.TabIndex = 3;
@@ -118,35 +120,57 @@
             // lb_Trim
             // 
             this.lb_Trim.AutoSize = true;
-            this.lb_Trim.Location = new System.Drawing.Point(260, 208);
+            this.lb_Trim.Location = new System.Drawing.Point(260, 231);
             this.lb_Trim.Name = "lb_Trim";
             this.lb_Trim.Size = new System.Drawing.Size(11, 12);
             this.lb_Trim.TabIndex = 4;
             this.lb_Trim.Text = "-";
             // 
-            // lb_Split
+            // lb_Split1
             // 
-            this.lb_Split.AutoSize = true;
-            this.lb_Split.Location = new System.Drawing.Point(47, 246);
-            this.lb_Split.Name = "lb_Split";
-            this.lb_Split.Size = new System.Drawing.Size(11, 12);
-            this.lb_Split.TabIndex = 1;
-            this.lb_Split.Text = "-";
+            this.lb_Split1.AutoSize = true;
+            this.lb_Split1.Location = new System.Drawing.Point(47, 269);
+            this.lb_Split1.Name = "lb_Split1";
+            this.lb_Split1.Size = new System.Drawing.Size(11, 12);
+            this.lb_Split1.TabIndex = 1;
+            this.lb_Split1.Text = "-";
             // 
             // lb_Split2
             // 
             this.lb_Split2.AutoSize = true;
-            this.lb_Split2.Location = new System.Drawing.Point(47, 284);
+            this.lb_Split2.Location = new System.Drawing.Point(47, 307);
             this.lb_Split2.Name = "lb_Split2";
             this.lb_Split2.Size = new System.Drawing.Size(11, 12);
             this.lb_Split2.TabIndex = 2;
             this.lb_Split2.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "실행하기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(339, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "초기화";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lb_Trim);
             this.Controls.Add(this.lb_Replace);
             this.Controls.Add(this.lb_ToUpper);
@@ -154,12 +178,13 @@
             this.Controls.Add(this.lb_ToLower);
             this.Controls.Add(this.lb_Substring);
             this.Controls.Add(this.lb_Split2);
-            this.Controls.Add(this.lb_Split);
+            this.Controls.Add(this.lb_Split1);
             this.Controls.Add(this.lb_Equals);
             this.Controls.Add(this.lb_Contain);
-            this.Controls.Add(this.lb_test);
+            this.Controls.Add(this.lb_Test);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,7 +192,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lb_test;
+        private System.Windows.Forms.Label lb_Test;
         private System.Windows.Forms.Label lb_Contain;
         private System.Windows.Forms.Label lb_Equals;
         private System.Windows.Forms.Label lb_Length;
@@ -176,7 +201,9 @@
         private System.Windows.Forms.Label lb_ToLower;
         private System.Windows.Forms.Label lb_ToUpper;
         private System.Windows.Forms.Label lb_Trim;
-        private System.Windows.Forms.Label lb_Split;
+        private System.Windows.Forms.Label lb_Split1;
         private System.Windows.Forms.Label lb_Split2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- WinForms/sm/Form3.cs
+++ WinForms/sm/Form3.cs
@@ -19,5 +19,49 @@ namespace sm
         {
 
         }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = lb_Test.Text; 
+            // 문자열이 안에 해당하는 문자열이 있는지 확인
+            lb_Contain.Text = str.Contains("Test").ToString();
+            // 문자열이 해당 문자열과 동일 한지 확인
+            lb_Equals.Text = str.Equals("Test").ToString();
+            // 개체의 문자수를 반환
+            lb_Length.Text = str.Length.ToString();
+            // 지정된 문자열을 다른 문자열로 변환
+            lb_Replace.Text = str.Replace("Test", "Good").ToString();
+            // 문자열에서 조건에 맞는 문자를 기준으로 분할
+            string[] strArr = str.Split('_');
+            lb_Split1.Text = strArr[0].ToString();
+            lb_Split2.Text = strArr[1].ToString();
+            // 문자열 내의 조건 위치의 부분 문자열을 검색
+            lb_Substring.Text = str.Substring(3,5).ToString();
+            // 문자열을 소문자로 변환
+            lb_ToLower.Text = str.ToLower().ToString();
+            // 문자열을 대문자로 변환
+            lb_ToUpper.Text = str.ToUpper().ToString();
+            // 문자열
+            lb_Trim.Text = str.Trim().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //초기화버튼
+        {
+            lb_Contain.Text = "-";
+            lb_Equals.Text = "-";
+            lb_Length.Text = "-";
+            lb_Replace.Text = "-";
+            lb_Split1.Text = "-";
+            lb_Split2.Text = "-";
+            lb_Substring.Text = "-";
+            lb_ToLower.Text = "-";
+            lb_ToUpper.Text = "-";
+            lb_Trim.Text = "-";
+        }
     }
 }
