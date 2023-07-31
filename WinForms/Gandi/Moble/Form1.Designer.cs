a1e288a45364536c2becad3da98fd67ee5fae435
tlagmlwns <simh4jun@gmail.com>
2023-07-31 11:38:57 +0900

    팀프 임시저장

---
+++ WinForms/Gandi/Moble/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Moble
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new System.Windows.Forms.Label();
+            progressBar1 = new System.Windows.Forms.ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Cooper Black", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.ForeColor = System.Drawing.Color.White;
+            label1.Location = new System.Drawing.Point(175, 35);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(146, 74);
+            label1.TabIndex = 0;
+            label1.Text = "200";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new System.Drawing.Point(12, 12);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new System.Drawing.Size(460, 30);
+            progressBar1.TabIndex = 1;
+            progressBar1.Value = 100;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label2.ForeColor = System.Drawing.Color.White;
+            label2.Location = new System.Drawing.Point(225, 100);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(52, 19);
+            label2.TabIndex = 2;
+            label2.Text = "score";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.RoyalBlue;
+            ClientSize = new System.Drawing.Size(484, 661);
+            Controls.Add(label2);
+            Controls.Add(progressBar1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+    }
+}
